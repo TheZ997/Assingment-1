d381aba6df3b6a0a9a3f0a23df09d09cb7885cd7
TheZ997 <sz227659@gmail.com>
2022-02-09 12:34:50 -0800

    Initial Commit

---
+++ assingment 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assingment 1
+//
+//  Created by Sergio Zavala on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

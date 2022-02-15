9f76602c6360d100726b7c3c56742a7eef8ea0f3
TheZ997 <sz227659@gmail.com>
2022-02-15 15:29:41 -0800

    updated files for part 1 and 2

---
+++ assingment 1/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  assingment 1
+//
+//  Created by Sergio Zavala on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
